b88af57202d65797accabae6d26a6d5d81462fa5
moveingsun <wyk1029@outlook.com>
2018-04-17 20:27:10 -0400

    The updated login api

--- flask_server/server.py
+++ flask_server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from controller.dao import get_user_info
 app = Flask(__name__)
 
@@ -7,12 +7,15 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+
 @app.route("/login/<username>/password/<password>", methods=['GET'])
 def valid(username, password):
     result = get_user_info(username, password)
     if result:
-        return result[2]
+        returndict = {'role':result[2]}
+        return jsonify(returndict)
     return 'Fail', 500
+
 
 if __name__ == '__main__':
     app.run( port=8000, debug=True)
