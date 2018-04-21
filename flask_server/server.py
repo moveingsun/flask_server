59c3f27c7a9c2f1b3e4a9312a84675c2e07f84bf
Xu Han <hanxu.workbox@gmail.com>
2018-04-20 20:54:10 -0400

    chaned server

--- flask_server/server.py
+++ flask_server/server.py
@@ -13,16 +13,22 @@ def valid(username, password):
     result = get_user_info(username, password)
     if result:
         returndict = {'role':result[2]}
-        return jsonify(returndict)
+        res = jsonify(returndict)
+        res.headers['Access-Control-Allow-Origin'] = '*'
+        return res
     return 'Fail', 500
+
 
 @app.route("/article/<title>/", methods=['GET'])
 def getarticle(title):
     result = get_article_info(title)
     if result:
         returndict = {'content':result[2]}
-        return jsonify(returndict)
+        res = jsonify(returndict)
+        res.headers['Access-Control-Allow-Origin'] = '*'
+        return res
     return 'Fail', 500
 
+
 if __name__ == '__main__':
-    app.run( port=8000, debug=True)
+    app.run(port=8000, debug=True)
